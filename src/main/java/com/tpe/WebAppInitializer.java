929d676d330e27a1a2f94a849ceeaa1bb8a4e528
muratkutlutuna <muratkutlutuna@gmail.com>
2024-03-25 07:11:09 +0100

    Intro without resource files

---
+++ src/main/java/com/tpe/WebAppInitializer.java
@@ -0,0 +1,35 @@
+package com.tpe;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//instead of web.xml we will use config classes
+
+//we need to register dispatcher servlet
+//AbstractAnnotationConfigDispatcherServletInitializer is used to start dispatcher servlet
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    /*
+    Dispatcher servlet Webcontext -> Controller, Handler Mapping, View Resolver(rebder)
+     */
+    @Override //DB and hibernate configuration
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+                RootContextConfig.class
+        };
+    }
+
+    @Override //MVC Configs (Controller, Handler Mapping, View Resolver(rebder))
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+                WebMvcConfig.class
+        };
+    }
+
+    @Override //servlet and url mapping
+    protected String[] getServletMappings() {
+        return new String[]{
+                "/" //all request will be received www.students/tecth
+        };
+    }
+}
---
+++ src/main/java/com/tpe/WebMvcConfig.java
@@ -0,0 +1,42 @@
+package com.tpe;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration // to make this a configuration class
+@ComponentScan("com.tpe") //to scan beans from the package specified
+@EnableWebMvc // we are telling we will use MVC
+public class WebMvcConfig  implements WebMvcConfigurer {
+
+    /**
+     * @return to add viewResolver obj as bean
+     */
+    @Bean
+    public InternalResourceViewResolver resolver() {
+
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class); //JavaStandartLibrary -->t to write less code inside jsp
+        resolver.setPrefix("/WEB-INF/views"); //indicating location of view files
+        resolver.setSuffix(".jsp"); //extensions of view files
+
+        return resolver();
+    }
+
+    /**
+     * we are setting the location of our resources (css, image, etc)
+     * for static resources we do not need to send this to dispatcher servlet
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**") //the path for static files
+                .addResourceLocations("/resources/") //this to indicate location
+                .setCachePeriod(0); //caching period
+    }
+}
